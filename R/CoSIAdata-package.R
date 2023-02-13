c1297e8a44ab436c60a91bd1b7fbb88906b4930d
Lasseigne Lab <lasseignelab@CDIB-Tabea-Soelter-MBP.local>
2023-02-13 11:48:00 -0600

    Outlined the man file for the package Created the Namespace Added RoxygenNote to Description Updated the Vignette Description of the Package

---
+++ R/CoSIAdata-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+#' @name CoSIAdata
+#' @aliases CoSIAdata
+#' @import ExperimentHub
+#' @description Variance Stabilized Transformation of Read Counts derieved from 
+#' Bgee RNA-Seq Expression Data. Expression Data includes annotations and is 
+#' across 6 species (Homo sapiens, Mus musculus, Rattus norvegicius,Danio rerio,
+#' Drosophila melanogaster, and Caenorhabditis elegans) and across more than 132
+#' tissues. The data is represented as a RData files and is available in 
+#' ExperimentHub.
+#' 
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
