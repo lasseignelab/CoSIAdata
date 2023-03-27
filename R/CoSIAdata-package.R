75f3aa3b841b1b17a9bce75d4b3e25737be7321a
anishahaldar <anisha.haldar1@gmail.com>
2023-03-27 17:34:12 +0000

    reformatting of the CoSIAdata-package.R file in order to decrease the lines over 80 and indentation
    updates to the NAMESPACE to export the .onLoad function

--- R/CoSIAdata-package.R
+++ R/CoSIAdata-package.R
@@ -2,26 +2,26 @@
 #' @name CoSIAdata
 #' @aliases CoSIAdata
 #' @import ExperimentHub
-#' @description 
-#' CoSIAdata includes Variance Stabilized Transformation of Read Counts from 
+#' @description
+#' CoSIAdata includes Variance Stabilized Transformation of Read Counts from
 #' Bgee RNA-Seq Expression Data across six species (Homo sapiens, Mus musculus,
-#' Rattus norvegicius,Danio rerio, Drosophila melanogaster, and Caenorhabditis 
-#' elegans) and more than 132 tissues. Each species has its own independent 
+#' Rattus norvegicius,Danio rerio, Drosophila melanogaster, and Caenorhabditis
+#' elegans) and more than 132 tissues. Each species has its own independent
 #' dataframe with its unique set of tissue and gene specific expression data.
-#' 
-#' CoSIAdata is meant to be integrated into the CoSIA Package, a visualization 
-#' tool for cross species comparison of expression metrics. However, it can be 
-#' used to conduct independent species, tissue, and gene-specific 
+#'
+#' CoSIAdata is meant to be integrated into the CoSIA Package, a visualization
+#' tool for cross species comparison of expression metrics. However, it can be
+#' used to conduct independent species, tissue, and gene-specific
 #' expression analysis.
-#' 
-#' Researchers might find this data useful as it provides Variance Stabilized 
+#'
+#' Researchers might find this data useful as it provides Variance Stabilized
 #' Transformation of read counts to compare RNA-Seq expression profiles.
 #' @import qpdf
 #' @format Multiple dataframes
-#' 
+#'
 #' @examples
 #' library(ExperimentHub)
-#' eh = ExperimentHub()
+#' eh <- ExperimentHub()
 #' query(eh, "CoSIAdata")
 #' eh[["EH7863"]]
 "_PACKAGE"
